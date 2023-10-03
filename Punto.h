10a9eaf4d57a244fe619cd01108e7022465b78fb
CesarOlarte <104476524+ElCzar@users.noreply.github.com>
2023-10-03 17:00:17 -0500

    Create Punto.h

---
+++ Punto.h
@@ -0,0 +1,19 @@
+struct Punto {
+    int x;
+    int y;
+    
+    Punto& operator = (const Punto &p) {
+    		x = p.x;
+        y = p.y;
+        return *this;
+    }
+    
+    bool operator == (const Punto &p) const {
+        return (x == p.x && y == p.y);
+    }
+    
+    friend std::ostream& operator << (std::ostream &o, const Punto &p) {
+        o << "(" << p.x << "," << p.y << ")";
+        return o;
+    }
+};
