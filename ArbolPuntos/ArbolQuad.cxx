422d778e72800526e2d5a37690ec27ddc0108c74
CesarOlarte <104476524+ElCzar@users.noreply.github.com>
2023-10-04 11:33:26 -0500

    Update ArbolQuad.cxx

--- ArbolPuntos/ArbolQuad.cxx
+++ ArbolPuntos/ArbolQuad.cxx
@@ -15,3 +15,38 @@ ArbolQuad::~ArbolQuad() {
     delete this->raiz;
     this->raiz = NULL;
 }
+
+Punto ArbolQuad::datoRaiz() {
+    if(!this->isVacio()) {
+        return this->raiz->getDato();
+    }
+    return NULL;
+}
+
+NodoQuad* ArbolQuad::getRaiz() {
+    return this->raiz;
+}
+
+void ArbolQuad::setRaiz(NodoQuad* n_raiz) {
+    this->raiz = n_raiz;
+}
+
+bool ArbolQuad::isVacio() {
+    return this->raiz == NULL;   
+}
+
+bool ArbolQuad::insert(Punto val) {
+    return false;
+}
+
+void ArbolQuad::preOrden() {
+    
+}
+
+void ArbolQuad::posOrden() {
+    
+}
+
+void ArbolQuad::nivelOrden() {
+
+}
